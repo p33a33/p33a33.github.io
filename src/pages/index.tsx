0ecadda73f71d659e6b3c8b074f69ac20b22fe6a
david <p33a33@gmail.com>
2022-09-19 22:44:50 +0900

    init resume

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,193 +1,274 @@
-import * as React from "react"
-import type { HeadFC } from "gatsby"
-
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const doclistStyles = {
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
-
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  display: `inline-block`,
-  marginBottom: 24,
-  marginRight: 12,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLinks = [
-  {
-    text: "TypeScript Documentation",
-    url: "https://www.gatsbyjs.com/docs/how-to/custom-configuration/typescript/",
-    color: "#8954A8",
-  },
-  {
-    text: "GraphQL Typegen Documentation",
-    url: "https://www.gatsbyjs.com/docs/how-to/local-development/graphql-typegen/",
-    color: "#8954A8",
-  }
-]
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative" as "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
-
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
+import * as React from "react";
+import type { HeadFC } from "gatsby";
+import "./resume.scss";
 
 const IndexPage = () => {
-  return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.tsx</code> to see this page
-        update in real-time. 😎
-      </p>
-      <ul style={doclistStyles}>
-        {docLinks.map(doc => (
-          <li key={doc.url} style={docLinkStyle}>
-            <a
-              style={linkStyle}
-              href={`${doc.url}?utm_source=starter&utm_medium=ts-docs&utm_campaign=minimal-starter-ts`}
-            >
-              {doc.text}
-            </a>
-          </li>
-        ))}
-      </ul>
-      <ul style={listStyles}>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter-ts`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
-    </main>
-  )
-}
+    return (
+        <main className="main">
+            <div className="container">
+                {/* 이력서 제목 */}
 
-export default IndexPage
+                {/* 간단한 자기소개 */}
+                <div className="section">
+                    <h1 className="resume-title">
+                        안녕하세요? 개발자 조성민입니다.
+                    </h1>
+                    <div className="content">
+                        <p className="intro">
+                            서울에서 2년차 프론트엔드 엔지니어로 일하고
+                            있습니다.
+                            <br />
+                            조직의 사업적 성과와 프로덕트의 개선에서 보람을
+                            느낍니다. <br />
+                            새로운 시도에 관대하고, 자기주도적으로 일할 수 있는
+                            조직에서 최고의 효율을 낼 수 있다고 생각합니다.{" "}
+                            <br />
+                            평생 실무자로 남는것을 목표로,
+                            <b> 하루하루 더 나은 코드</b>를 쓸 수 있도록 배움을
+                            거듭해 나갑니다.
+                        </p>
+                    </div>
+                </div>
 
-export const Head: HeadFC = () => <title>Home Page</title>
+                {/* 경력사항 */}
+                <div className="section">
+                    <h2 className="section-title">경력사항</h2>
+
+                    <div className="content">
+                        <div className="left">
+                            <h3>에이팀벤처스</h3>{" "}
+                            <div className="period">(2021. 3. ~ 현재)</div>
+                            <div style={{ fontSize: 16, fontWeight: 500 }}>
+                                Frontend Engeenier
+                            </div>
+                        </div>
+                        <div className="right">
+                            <div className="project">
+                                <h4>
+                                    제조업 매칭 플랫폼 <b>CAPA</b>
+                                </h4>
+                                <div className="item-title">프로젝트 설명</div>
+                                <div className="item-description">
+                                    제조가 필요한 고객이 견적을 요청하고,
+                                    제조능력이 있는 파트너와 연결되는
+                                    플랫폼입니다. 파트너와 도면을 보면서 채팅을
+                                    통해 상담할 수 있습니다.
+                                </div>
+                                <div className="item-title">주요 작업내용</div>
+                                <div className="item-description">
+                                    <ul>
+                                        <li>
+                                            웹용 도면뷰어 <b>CAPA Viewer</b>를
+                                            개발하는 R&D팀과 협업하여 고객과
+                                            파트너가 뷰어를 통해 특정 부품이나
+                                            부분에 주석을 남기고, 댓글을 남길
+                                            수있는 기능을 개발했습니다.
+                                        </li>
+                                        <li>
+                                            <b> 견적요청서 작성 UI와 로직</b>을
+                                            개발했습니다.
+                                        </li>
+                                        <li>
+                                            신규 제조서비스를 위한
+                                            <b> 견적요청서</b>를 개발했습니다.
+                                            신규요청서에{" "}
+                                            <b>새로운 레이아웃의 UI를 구현</b>
+                                            하여 HACKLE을 사용해 A/B테스트를
+                                            진행했습니다.
+                                        </li>
+                                        <li>
+                                            파트너에게 받은 견적을 확정하고 해당
+                                            파트너에게{" "}
+                                            <b>프로젝트를 발주하는 기능</b>을
+                                            개발했습니다.
+                                        </li>
+                                        <li>
+                                            서비스의
+                                            <b> 랜딩페이지 UI 개편</b>을
+                                            진행했습니다.
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div className="item-title">기술 스택</div>
+                                <div className="item-description">
+                                    Typescript, React, Redux, Apollo client,
+                                    Graphql, Material-ui, Styled-components,
+                                    PostMessage API
+                                </div>
+                            </div>
+
+                            <div className="project">
+                                <h4>
+                                    제조서비스 공급자용 서비스
+                                    <b> CAPA PARTNERS</b>
+                                </h4>
+                                <div className="item-title">프로젝트 설명</div>
+                                <div className="item-description">
+                                    CAPA를 통해 요청한 고객들의 프로젝트에
+                                    견적서를 제출하고, 발주까지 진행할 수 있는
+                                    서비스입니다.
+                                </div>
+                                <div className="item-title">주요 작업내용</div>
+                                <div className="item-description">
+                                    <ul>
+                                        <li>
+                                            가격경쟁을 넘어 파트너의 역량을 잘
+                                            보여줄 수 있는{" "}
+                                            <b>파트너 회사 소개서</b> 기능을
+                                            개발했습니다.
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div className="item-title">기술 스택</div>
+                                <div className="item-description">
+                                    Typescript, React, Redux, Apollo client,
+                                    Graphql, Material-ui, Styled-components
+                                </div>
+                            </div>
+
+                            <div className="project">
+                                <h4>
+                                    도면 클라우드 저장소 서비스
+                                    <b> CAPA CLOUD </b>
+                                </h4>
+                                <div className="item-title">프로젝트 설명</div>
+                                <div className="item-description">
+                                    도면 파일을 클라우드에 저장하고, 웹에서
+                                    2D/3D 도면을 조회할 수 있는 서비스입니다.
+                                    현재는 CLOUD 라는 서비스로 제공중이지
+                                    않으며, 이 프로젝트를 기반으로
+                                    <b> CAPA CONNECT</b> 서비스가
+                                    런칭되었습니다.
+                                </div>
+                                <div className="item-title">주요 작업내용</div>
+                                <div className="item-description">
+                                    <ul>
+                                        <li>
+                                            Axios 기반으로
+                                            <b> 복수 파일의 병렬 업로드</b>를
+                                            구현하고,{" "}
+                                            <b>
+                                                업로드 진행상황을 표시하는 UI를
+                                                개발
+                                            </b>
+                                            했습니다.
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div className="item-title">기술 스택</div>
+                                <div className="item-description">
+                                    Typescript, React, Apollo Client, Axios,
+                                    Graphql
+                                </div>
+                            </div>
+
+                            <div className="project">
+                                <h4>개발환경/문화 기여</h4>
+                                <div className="item-title">프로젝트 설명</div>
+                                <div className="item-description">
+                                    주어진 태스크 외에 별개로 추가적으로
+                                    개발환경이나 개발문화의 개선을 위해
+                                    주도적으로 진행한 프로젝트입니다.
+                                </div>
+                                <div className="item-title">주요 작업내용</div>
+                                <div className="item-description">
+                                    <h5>개발환경 / 업무환경 개선</h5>
+                                    <ul>
+                                        <li>
+                                            모든 프론트 프로젝트에
+                                            <b> 파일 경로 Alias를 적용</b>하여,
+                                            파일 가독성을 개선했습니다.
+                                        </li>{" "}
+                                        <li>
+                                            프로젝트
+                                            <b>
+                                                {" "}
+                                                webpack 설정에 SWC-loader를 적용
+                                            </b>
+                                            하여, 빌드시간을 개선했습니다.
+                                        </li>
+                                        <li>
+                                            백오피스에{" "}
+                                            <b>테스트 계정 관리 페이지</b>와{" "}
+                                            컨텐츠팀의{" "}
+                                            <b>
+                                                자료 등록을 위한 페이지를 개발
+                                            </b>
+                                            했습니다.
+                                        </li>
+                                    </ul>
+
+                                    <h5>개발문화 개선</h5>
+                                    <ul>
+                                        <li>
+                                            주도적으로{" "}
+                                            <b>Git branch 관리 전략을 설정</b>
+                                            했습니다.
+                                        </li>
+                                        <li>
+                                            Merge를 위해{" "}
+                                            <b>Pull Request 규칙을 설정</b>하고,{" "}
+                                            <b>
+                                                코드 리뷰를 통한 PR 승인을
+                                                적용하여
+                                            </b>{" "}
+                                            개발 문화 개선에 기여했습니다.
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div className="item-title">기술 스택</div>
+                                <div className="item-description">
+                                    <ul>
+                                        <li>
+                                            파일 경로 Alias : Typescript complie
+                                            option
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="section">
+                    <h2 className="section-title">교육</h2>
+
+                    <div className="content">
+                        <div className="left">
+                            <h3>코드스테이츠</h3>
+                            <br />
+                            <div className="period">(2020. 6. ~ 2020. 11.)</div>
+                        </div>
+                        <div className="right">
+                            <div className="project">
+                                <h4>Software Engeenier Course</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                {/* 학력 */}
+                <div className="section">
+                    <h2 className="section-title">학력</h2>
+
+                    <div className="content">
+                        <div className="left">
+                            <h3>평택대학교</h3>
+                            <br />
+                            <div className="period">(2010. 3. ~ 2016. 8.)</div>
+                        </div>
+                        <div className="right">
+                            <div className="project">
+                                <h4>경영학과 학사</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+    );
+};
+
+export default IndexPage;
+
+export const Head: HeadFC = () => <title>Home Page</title>;
